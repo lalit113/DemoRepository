8724dfdacdfb68434c5640b6d8c7af22511505e4
lalit113 <lalit24899@gmail.com>
2022-06-06 13:32:56 +0530

    Added for Loop

--- Abc.java
+++ Abc.java
@@ -1,5 +1,8 @@
 public class Abc {
 	public static void main(String[] args) {
-		system.out.println("Hello Everybody");
+		for(int i=1; i<5; i++)
+		{
+			system.out.println("Hello Everybody");
+		}
 	}
 }
